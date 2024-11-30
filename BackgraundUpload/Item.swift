1d04f998eb66b6b29e1e2e67f59b3e1fe46a327b
d.nareyko <d.nareyko@itransition.com>
2024-11-30 14:39:46 +0100

    Initial Commit

---
+++ BackgraundUpload/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  BackgraundUpload
+//
+//  Created by Nareyko, Diana on 30.11.24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
